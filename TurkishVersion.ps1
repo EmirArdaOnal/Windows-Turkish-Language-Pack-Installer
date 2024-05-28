a1e0aaec53d133cb4dc7dd02dedc5a5f4c5f315c
Emir Arda <127487311+EmirArdaOnal@users.noreply.github.com>
2024-05-28 08:55:35 +0300

    Update TurkishVersion.ps1

--- TurkishVersion.ps1
+++ TurkishVersion.ps1
@@ -1,31 +1,41 @@
+
 ########################################################
-## Çalışan Windows İmajına Türkçe Dil Paketi Ekle ##
+## Add English Language Pack to running Windows Image ##
 ########################################################
 
-## Dil Paketi Temizliğini Devre Dışı Bırak ##
-Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Ön yüklü uygulama temizliği"
+$Language = "tr-TR"
+Add-WindowsCapability -Online -Name "Language.Basic~~~$Language~0.0.1.0"
+Add-WindowsCapability -Online -Name "Language.Handwriting~~~$Language~0.0.1.0"
+Add-WindowsCapability -Online -Name "Language.OCR~~~$Language~0.0.1.0"
+Add-WindowsCapability -Online -Name "Language.Speech~~~$Language~0.0.1.0"
+Set-WinUILanguageOverride -Language $Language
+Set-WinUserLanguageList $Language -Force
+Set-WinSystemLocale $Language
+Set-Culture $Language
 
-## Dil Paketi İçerik Depolama Yerini Ayarla ##
-[string]$LIPContent = "E:"
+$Language = "tr-TR"
+# Dil paketi indirme ve yükleme
+Add-WindowsCapability -Online -Name "Language.Basic~~~$Language~0.0.1.0"
+# İsteğe bağlı dil özelliklerini yükleyin
+Add-WindowsCapability -Online -Name "Language.Handwriting~~~$Language~0.0.1.0"
+Add-WindowsCapability -Online -Name "Language.OCR~~~$Language~0.0.1.0"
+Add-WindowsCapability -Online -Name "Language.Speech~~~$Language~0.0.1.0"
+# Sistem dilini yapılandırma
+Set-WinUILanguageOverride -Language $Language
+Set-WinUILanguageFallback -Language $Language
+Set-WinUserLanguageList $Language -Force
+Set-WinSystemLocale $Language
+Set-Culture $Language
 
-## Türkçe Dil Paketi Ekle ##
-Add-AppProvisionedPackage -Online -PackagePath $LIPContent\tr-tr\LanguageExperiencePack.tr-tr.Neutral.appx -LicensePath $LIPContent\tr-tr\Lisans.xml
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-Client-Language-Pack_x64_tr-tr.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-Basic-tr-tr-Package~31bf3856ad364e35~amd64~~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-Handwriting-tr-tr-Package~31bf3856ad364e35~amd64~~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-OCR-tr-tr-Package~31bf3856ad364e35~amd64~~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-Speech-tr-tr-Package~31bf3856ad364e35~amd64~~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-TextToSpeech-tr-tr-Package~31bf3856ad364e35~amd64~~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-MSPaint-FoD-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-Notepad-FoD-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-PowerShell-ISE-FOD-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-Printing-WFS-FoD-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-StepsRecorder-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-WordPad-FoD-Package~31bf3856ad364e35~amd64~tr-tr~.cab
-
-## Kullanıcı Dil Listesine Türkçe Dili Ekle ##
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("tr-TR")
-Set-WinUserLanguageList $LanguageList -force
+Set-WinUserLanguageList $LanguageList -Force
+if ($?) {
+    Write-Host "Dil paketi başarıyla eklendi. Ayarlar kısmından dilin üstüne tıklayıp 'Seçenekler' ve sonra 'Dil paketini indir'e basınız."
+} else {
+    Write-Host "Dil paketi eklenirken bir hata oluştu."
+}
+
+
+# Yeniden başlatma gerekebilir
+Restart-Computer
